43d26e25b2526de5a18a900da0becdc6a652e5ca
avannoo <axelvan22@hotmail.fr>
2023-08-25 11:23:29 +0200

    add comments

--- embroidery-visualizer/src/Editor.js
+++ embroidery-visualizer/src/Editor.js
@@ -4,70 +4,31 @@ import React, { useState, useEffect , useRef} from 'react';
 
 function Editor(props) {
 
+    // initialize canvas and context
     const canvasRef = useRef(null);
     const canvasCtxRef = useRef(null);
+
+    // imageSrc refers to the image data contained in the canvas
     const [imageSrc, setImageSrc] = useState(null);
-    //const [imageSelected, setImageSelected] = useState(false);
-
-    const handleInputChange = (event) => {
-      //setImageSelected(true);
-      const reader = new FileReader();
-      reader.onloadend = (event) => {
-        const src_image = new Image();
-  
-        src_image.onload = () => {
-          if (canvasRef.current && canvasCtxRef.current) {
-
-            var hRatio = canvasRef.current.width / src_image.width    ;
-            var vRatio = canvasRef.current.height / src_image.height  ;
-            var ratio  = Math.min ( hRatio, vRatio );
-
-            canvasCtxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-            canvasCtxRef.current.drawImage(src_image, 0, 0, src_image.width, src_image.height, 0,0,src_image.width*ratio, src_image.height*ratio);
-            var imageData = canvasRef.current.toDataURL("image/jpg");
-            setImageSrc(imageData);
-            console.log(imageData);
-          }
-        };
-        src_image.src = event.target.result;
-      };
-      reader.readAsDataURL(event.target.files[0]);
-    };
 
     useEffect(() => {
-        if (canvasRef.current) {
-          // resize image to canvas
-          canvasRef.current.height = "500";
-          canvasRef.current.width = "500";
+      if (canvasRef.current) {
+        // set the canvas size
+        canvasRef.current.height = "500";
+        canvasRef.current.width = "500";
 
-          canvasCtxRef.current = canvasRef.current.getContext("2d");
-          canvasCtxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-          if(imageSrc !== null){
-            const image = new Image();
-            image.src = imageSrc;
-            image.onload = () => {
-              canvasCtxRef.current.drawImage(image, 0, 0, 500, 500);
-            }
+        // set the canvas' context
+        canvasCtxRef.current = canvasRef.current.getContext("2d");
+
+        // print the image in the canvas when it's loaded
+        if(imageSrc !== null){
+          const image = new Image();
+          image.src = imageSrc;
+          image.onload = () => {
+            canvasCtxRef.current.drawImage(image, 0, 0,);
           }
+        }
       };
-        
-        /*const hoveredColor = document.getElementById('hovered-color');
-        const selectedColor = document.getElementById('selected-color');
-
-
-        function pick(event, destination) {
-        const bounding = canvasRef.current.getBoundingClientRect();
-        const x = event.clientX - bounding.left;
-        const y = event.clientY - bounding.top;
-        const pixel = canvasCtxRef.current.getImageData(x, y, 1, 1);
-        const data = pixel.data;
-
-        const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-        destination.style.background = rgba;
-        destination.textContent = rgba;
-
-        return rgba;
-        }*/
 
         /*const moveHandler = () =>{
           console.log("Mouse moved on canvas");
@@ -83,7 +44,34 @@ function Editor(props) {
           canvasRef.current.removeEventListener('click', clickHandler);
           canvasRef.current.removeEventListener('mousemove', moveHandler);
         }*/
-    }, [imageSrc]);
+      }, [imageSrc]);
+
+    // this function is called to load the image file
+    const handleInputChange = (event) => {
+      const reader = new FileReader();
+      reader.onloadend = (event) => {
+        const src_image = new Image();
+  
+        src_image.onload = () => {
+          if (canvasRef.current && canvasCtxRef.current) {
+            // calculate the size of the image for it to fit in the canvas
+            var hRatio = canvasRef.current.width / src_image.width    ;
+            var vRatio = canvasRef.current.height / src_image.height  ;
+            var ratio  = Math.min ( hRatio, vRatio );
+
+            // clear the canvas before drawing the loaded image 
+            canvasCtxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            canvasCtxRef.current.drawImage(src_image, 0, 0, src_image.width, src_image.height, 0,0,src_image.width*ratio, src_image.height*ratio);
+            var imageData = canvasRef.current.toDataURL("image/jpg");
+
+            // set the new imageSrc value
+            setImageSrc(imageData);
+          }
+        };
+        src_image.src = event.target.result;
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    };
 
     return (
       <>
