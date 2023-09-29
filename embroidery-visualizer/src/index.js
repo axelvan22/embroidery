80b0d4a65a6108fbd586d852c4ef6009e85fee82
avannoo <axelvan22@hotmail.fr>
2023-09-29 16:39:55 +0200

    update index.js

--- embroidery-visualizer/src/index.js
+++ embroidery-visualizer/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import Visualizer from './Visualizer';
 import Editor from './Editor.js';
 import './index.css';
-
+import { Canvas } from './Canvas.js';
+import DrawRectangle from './DrawRectangle.js'; 
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Editor />
-      <Visualizer />
   </React.StrictMode>
 );
